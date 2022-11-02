87e817511a308fd13a90997f553c4f36f706aa1a
Viktor-117 <viktor.hushchin@gmail.com>
2022-11-02 20:36:57 +0200

    final update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,7 +5,7 @@ import Section from 'components/Section';
 import Notification from 'components/Notification';
 import { Container } from './App.styled';
 
-function App() {
+export default function App() {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
@@ -63,5 +63,3 @@ function App() {
     </Container>
   );
 }
-
-export { App };
